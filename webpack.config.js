25c4373b86249bd4b18a2d1d219ee37392d169f6
Petr Dvukhrechensky <petr.sdkb@gmail.com>
2020-04-23 22:27:03 +0700

    Refactor webpack config to support environments. Remove source maps from prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,55 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                loader: 'babel-loader',
-                test: /\.js$/, 
-                exclude: /node_modules/
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        port: 3000,
-        historyApiFallback: true,
+module.exports = (env, argv) => {
+    const isProduction = env === 'production'
+    
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        mode: 'development',
+        module: {
+            rules: [
+                {
+                    loader: 'babel-loader',
+                    test: /\.js$/, 
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.s?css$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'styles.css'
+            })
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            port: 3000,
+            historyApiFallback: true,
+        }
     }
 }
